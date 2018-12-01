41a0556f713c7f78ba70dc11cf7e9d73c74b890a
HCLivess <admin@bismuth.cz>
2018-12-01 10:58:33 -0600

    update readme

--- autogame/autogame.py
+++ autogame/autogame.py
@@ -7,13 +7,40 @@ c = conn.cursor()
 
 block = 1
 
+
 class Hero:
+    FULL_HP = 500
+
     def __init__(self):
-        self.health = 100
+        self.health = self.FULL_HP
         self.power = 10
         self.alive = True
         self.in_combat = False
         self.experience = 0
+
+class Troll:
+    def __init__(self):
+        self.health = 20
+        self.power = 15
+        self.alive = True
+
+class Goblin:
+    def __init__(self):
+        self.health = 10
+        self.power = 10
+        self.alive = True
+
+class Berserk:
+    def __init__(self):
+        self.health = 5
+        self.power = 30
+        self.alive = True
+
+class Dragon:
+    def __init__(self):
+        self.health = 30
+        self.power = 50
+        self.alive = True
 
 hero = Hero()
 
@@ -36,6 +63,18 @@ triggers = {"4f" : "troll",
             "61a" : "dragon"
             }
 
+def enemy_define(event):
+    if event == "troll":
+        enemy = Troll()
+    elif event == "goblin":
+        enemy = Goblin()
+    elif event == "berserk":
+        enemy = Berserk()
+    elif event == "dragon":
+        enemy = Dragon()
+
+    return enemy
+
 def cycle(block):
     c.execute("SELECT * FROM transactions WHERE block_height = ? ORDER BY block_height", (block,))
     result = c.fetchall()
@@ -49,43 +88,49 @@ while hero.alive:
     for trigger_key in triggers:
 
         if trigger_key in block_hash and hero.alive:
-            print("You meet {}".format(triggers[trigger_key]))
-
-
+            trigger = triggers[trigger_key]
+            print("You meet {}".format(trigger))
             hero.in_combat = True
-            while hero.alive and hero.in_combat:
+
+            enemy = enemy_define(trigger)
+
+            while hero.alive and enemy.alive:
                 block_hash = cycle(block)
 
                 for event_key in events:
-                    if event_key in block_hash and hero.alive:
-                        print("Event: {}".format(events[event_key]))
+                    if event_key in block_hash:
+                        event = events[event_key]
+                        print("Event: {}".format(event))
 
-                        if events[event_key] == "attack":
-                            print ("{} killed! You stride forward with {} HP...".format(triggers[trigger_key], hero.health))
+                        if event == "attack":
                             hero.experience += 1
-                            hero.in_combat = False
-                            break
+                            enemy.health -= hero.power
+                            print("Enemy suffers {} damage and is left with {} HP".format(hero.power, enemy.health))
 
-                        if events[event_key] == "heal":
-                            if hero.in_combat and hero.health < 100:
+                            if enemy.health < 1:
+                                hero.in_combat = False
+                                enemy.alive = False
+                                print ("Enemy dies")
+                                break
 
-                                if hero.in_combat:
-                                    hero.health = hero.health + 5
-                                    print("You drink a potion and heal to {} HP...".format(hero.health))
+                        if event == "heal":
+                            if hero.in_combat and hero.health < Hero.FULL_HP:
+                                hero.health = hero.health + 5
+                                print("You drink a potion and heal to {} HP...".format(hero.health))
 
-                                else:
-                                    hero.health = 100
-                                    print ("You rest and fully heal...")
+                            elif not hero.in_combat:
+                                hero.health = 100
+                                print ("You rest and fully heal...")
 
-                                if hero.health > 100:
-                                    hero.health = 100
+                            if hero.health > Hero.FULL_HP:
+                                hero.health = Hero.FULL_HP
 
                             break
 
                         if events[event_key] == "attacked":
                             hero.in_combat = True
-                            hero.health = hero.health - 20
-                            print("{} hit you for 20 HP, you now have {} HP".format(triggers[trigger_key], hero.health))
+                            hero.health = hero.health - enemy.power
+                            print("{} hit you for {} HP, you now have {} HP".format(trigger, enemy.power, hero.health))
                             if hero.health < 1:
                                 print("You died with {} experience".format(hero.experience))
                                 hero.alive = False
