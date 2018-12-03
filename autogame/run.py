6c2b53c837a84861875f39ddc0cda1f5c20d95a0
HCLivess <admin@bismuth.cz>
2018-12-02 22:37:47 -0600

    automated

--- autogame/run.py
+++ autogame/run.py
@@ -2,27 +2,31 @@ import sqlite3
 import core
 import classes
 import json
+import time
 
 db = core.db #do not reinit
 scores_db = classes.ScoreDb()
 
-db.c.execute("SELECT * FROM transactions WHERE operation = ?",("autogame",))
-result = db.c.fetchall()
-print (result)
+while True:
+    db.c.execute("SELECT * FROM transactions WHERE operation = ?",("autogame",))
+    result = db.c.fetchall()
+    print (result)
 
-for entry in result:
+    for entry in result:
 
-    seed = entry[2]  # address
-    block = entry[0]
+        seed = entry[2]  # address
+        block = entry[0]
 
-    game,hero = core.go(seed, block)
+        game,hero = core.go(seed, block)
 
-    if game.finished:
+        if game.finished:
 
-        try:
-            scores_db.c.execute("SELECT * FROM scores WHERE hash = ?", (game.hash,))
-            dummy = scores_db.c.fetchall()[0]
-        except:
-            scores_db.c.execute("INSERT INTO scores VALUES (?,?,?,?,?)", (game.block,game.hash,game.seed,hero.experience,json.dumps(hero.inventory),))
-            scores_db.conn.commit()
+            try:
+                scores_db.c.execute("SELECT * FROM scores WHERE hash = ?", (game.hash,))
+                dummy = scores_db.c.fetchall()[0]
+            except:
+                scores_db.c.execute("INSERT INTO scores VALUES (?,?,?,?,?)", (game.block,game.hash,game.seed,hero.experience,json.dumps(hero.inventory),))
+                scores_db.conn.commit()
 
+    print ("Run finished, sleeping")
+    time.sleep(60)
