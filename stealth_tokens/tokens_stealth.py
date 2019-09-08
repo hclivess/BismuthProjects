ad22b4ecbdfb0459ecf0c677080fe811875ea287
HCLivess <admin@bismuth.cz>
2019-09-08 15:51:39 +0200

    optimize

--- stealth_tokens/tokens_stealth.py
+++ stealth_tokens/tokens_stealth.py
@@ -19,14 +19,17 @@ def signals_generate(size):
 def token_key_generate(len=32): #AES-256 default
     return get_random_bytes(len)
 
-def encrypt_data(token_name, token_amount, operation, key):
+def encrypt_data(token_name, token_amount, operation, key_encoded):
+    key = b64decode(key_encoded)
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     ciphertext, tag = cipher.encrypt_and_digest(json.dumps({"name": token_name.zfill(10), "amount": token_amount.zfill(10), "operation":operation}).encode())
 
     return {"nonce": b64encode(nonce).decode(), "ciphertext": b64encode(ciphertext).decode(),"tag": b64encode(tag).decode()}
 
-def decrypt(enc_dict, key):
+def decrypt(enc_dict, key_encoded):
+    key = b64decode(key_encoded)
+
     cipher = AES.new(key, AES.MODE_EAX, nonce=b64decode(enc_dict["nonce"]))
 
     plaintext = json.loads(cipher.decrypt(b64decode(enc_dict["ciphertext"])).decode())
@@ -41,17 +44,16 @@ def decrypt(enc_dict, key):
     except ValueError:
         print("Key incorrect or message corrupted")
 
-def load_token_key(token):
-    with open('token_keys.json') as token_keys:
+def load_token_dict(token):
+    with open(f'{token}_keys.json') as token_keys:
         keys_loaded = json.loads(token_keys.read())
-    for key, value in keys_loaded.items():
-        if key == token:
-            return {"token": token, "token key": b64decode(value)}
+    return keys_loaded
 
 def save_token_key(token, signals, key):
     if not os.path.exists(f'{token}_keys.json'):
         keys = {}
-        keys["keys"] = b64encode(key).decode()
+        keys["name"] = token
+        keys["key"] = b64encode(key).decode()
         keys["signals"] = signals
 
         with open(f'{token}_keys.json',"w") as token_keys:
@@ -72,26 +74,28 @@ if __name__ == "__main__":
     address = "fa442ebb19292114f4f9d53a72c6b396472c7971b9de598bc9d0b4cd"
 
     #print("test", token_key_generate())
-    save_token_key(token="stest", signals=signals_generate(10), key=token_key_generate())
+    save_token_key(token="stest", signals=signals_generate(100), key=token_key_generate())
 
-    token_key_dict = load_token_key(token=stealth_token)
-    print("token_key", token_key_dict)
+    token_key_dict = load_token_dict(token=stealth_token)
+    print("token_key_dict", token_key_dict)
 
-    address_token_key_dict = address_token_key_generate(token_key=token_key_dict["token key"], address=address)
+    address_token_key_dict = address_token_key_generate(token_key=token_key_dict["name"], address=address)
     print("address_token_key", address_token_key_dict)
     # stealth tokens
 
     #print("stoken:make")
     #print("stest:10000")
 
-    encrypted_data_make = encrypt_data(token_name="stest", token_amount="10000", operation="make", key=token_key_dict["token key"])
-    encrypted_data_move = encrypt_data(token_name="stest", token_amount="1", operation="move", key=token_key_dict["token key"])
+    encrypted_data_make = encrypt_data(token_name="stest", token_amount="10000", operation="make", key_encoded=token_key_dict["key"])
+    encrypted_data_move = encrypt_data(token_name="stest", token_amount="1", operation="move", key_encoded=token_key_dict["key"])
 
-    print("encrypted_data_make", encrypted_data_make)
-    print(decrypt(encrypted_data_make, token_key_dict["token key"]))
+    print(decrypt(encrypted_data_make, token_key_dict["key"]))
+    print("make (data)", encrypted_data_make)
+    print("make (operation)", random.choice(token_key_dict["signals"]))
 
-    print("encrypted_data_move", encrypted_data_move)
-    print(decrypt(encrypted_data_move, token_key_dict["token key"]))
+    print(decrypt(encrypted_data_move, token_key_dict["key"]))
+    print("move (data)", encrypted_data_move)
+    print("move (operation)", random.choice(token_key_dict["signals"]))
 
     #index var is automatically overwritten in regmode
 
