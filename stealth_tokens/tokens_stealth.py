838d0c8a0cfc258f23e48b0e52453fe64b104732
HCLivess <admin@bismuth.cz>
2019-09-12 22:29:14 +0200

    update

--- stealth_tokens/tokens_stealth.py
+++ stealth_tokens/tokens_stealth.py
@@ -19,11 +19,11 @@ def signals_generate(size):
 def token_key_generate(len=32): #AES-256 default
     return get_random_bytes(len)
 
-def encrypt_data(token_name, token_amount, operation, key_encoded):
+def encrypt_data(token_name, token_amount, operation, recipient, key_encoded):
     key = b64decode(key_encoded)
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
-    ciphertext, tag = cipher.encrypt_and_digest(json.dumps({"name": token_name.zfill(10), "amount": token_amount.zfill(10), "operation":operation}).encode())
+    ciphertext, tag = cipher.encrypt_and_digest(json.dumps({"name": token_name.zfill(10), "amount": token_amount.zfill(10), "recipient": recipient, "operation": operation}).encode())
 
     return {"nonce": b64encode(nonce).decode(), "ciphertext": b64encode(ciphertext).decode(),"tag": b64encode(tag).decode()}
 
@@ -59,19 +59,19 @@ def save_token_key(token, signals, key):
         with open(f'{token}_keys.json',"w") as token_keys:
             token_keys.write(json.dumps(keys))
 
-def address_token_key_generate(token_key, address):
-    address_token_key = blake2b(repr((token_key,address)).encode(), digest_size=7).hexdigest()
-    return {"address":address, "address token key": address_token_key}
+#def slave_key_generate(token_key, address):
+#    address_slave_key = blake2b(repr((token_key, address)).encode(), digest_size=7).hexdigest()
+#    return {"address":address, "address slave key": address_slave_key}
 
 def tokens_update():
     pass
-
 
 if __name__ == "__main__":
 
     #stealth tokens
     stealth_token = "stest"
     address = "fa442ebb19292114f4f9d53a72c6b396472c7971b9de598bc9d0b4cd"
+    recipient = "46fa2d2f7a8ed7221aa292e36e2c0bfdc6e53e0a4bd65e34694dc94b"
 
     #print("test", token_key_generate())
     save_token_key(token="stest", signals=signals_generate(100), key=token_key_generate())
@@ -79,15 +79,15 @@ if __name__ == "__main__":
     token_key_dict = load_token_dict(token=stealth_token)
     print("token_key_dict", token_key_dict)
 
-    address_token_key_dict = address_token_key_generate(token_key=token_key_dict["name"], address=address)
-    print("address_token_key", address_token_key_dict)
+    #address_token_key_dict = slave_key_generate(token_key=token_key_dict["name"], address=address)
+    #print("address_token_key", address_token_key_dict)
     # stealth tokens
 
     #print("stoken:make")
     #print("stest:10000")
 
-    encrypted_data_make = encrypt_data(token_name="stest", token_amount="10000", operation="make", key_encoded=token_key_dict["key"])
-    encrypted_data_move = encrypt_data(token_name="stest", token_amount="1", operation="move", key_encoded=token_key_dict["key"])
+    encrypted_data_make = encrypt_data(token_name="stest", token_amount="10000", recipient=recipient, operation="make", key_encoded=token_key_dict["key"])
+    encrypted_data_move = encrypt_data(token_name="stest", token_amount="1", recipient=recipient, operation="move", key_encoded=token_key_dict["key"])
 
     print(decrypt(encrypted_data_make, token_key_dict["key"]))
     print("make (data)", encrypted_data_make)
